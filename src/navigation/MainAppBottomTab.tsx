eb91ff1c0b6df7aef84285533ab1c298c9251d8b
kamalalshinawi <kamalalshinawi@gmail.com>
2026-03-03 23:02:54 +0200

    style(navigation): customize bottom tab appearance and icons

--- src/navigation/MainAppBottomTab.tsx
+++ src/navigation/MainAppBottomTab.tsx
@@ -2,7 +2,10 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CartScreen from "../screens/cart/CartScreen";
 import ProfileScreen from "../screens/profile/ProfileScreen";
 import HomeScreen from "../screens/home/HomeScreen";
-
+import { AppColors } from "../styles/colors";
+import {s,vs} from "react-native-size-matters";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Octicons from '@expo/vector-icons/Octicons';
 const Tab = createBottomTabNavigator();
 
 const MainAppBottomTab = () => {
@@ -10,11 +13,39 @@ const MainAppBottomTab = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: AppColors.primary,
+        tabBarInactiveTintColor: AppColors.disabledGray,
+        tabBarLabelStyle:{
+          fontSize:s(12),
+          marginTop: vs(4)
+        }
       }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Cart" component={CartScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} 
+      options={{
+        tabBarIcon: ({color,size})=>(
+          <AntDesign name="home" size={24} color={color} />
+        ),
+        title: "Home"
+      }}
+      
+      />
+      <Tab.Screen name="Cart" component={CartScreen} 
+      options={{
+        tabBarIcon: ({color,size}) => (
+          <AntDesign name="shopping-cart" size={size} color={color} />
+        ),
+        title: "Cart"
+      }}
+      />
+      <Tab.Screen name="Profile" component={ProfileScreen} 
+      options={{
+        tabBarIcon: ({color,size}) => (
+          <Octicons name="person" size={size} color={color} />
+        ),
+        title: "Profile"
+      }}
+      />
     </Tab.Navigator>
   );
 };
