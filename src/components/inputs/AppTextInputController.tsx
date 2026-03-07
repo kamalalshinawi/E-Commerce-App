3c2abf777c79962453b9d1811ba7639bda2e0501
kamalalshinawi <kamalalshinawi@gmail.com>
2026-03-07 23:17:26 +0200

    style: style Error Message

--- src/components/inputs/AppTextInputController.tsx
+++ src/components/inputs/AppTextInputController.tsx
@@ -1,7 +1,10 @@
-import {StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import { Controller } from "react-hook-form";
 import AppTextInput from "./AppTextInput";
+import { AppColors } from "../../styles/colors";
+import AppText from "../texts/AppText";
+import { vs } from "react-native-size-matters";
 const AppTextInputController = ({
   control,
   name,
@@ -16,20 +19,34 @@ const AppTextInputController = ({
       name={name}
       rules={rules}
       render={({ field: { onChange, value }, fieldState: { error } }) => (
-         <AppTextInput
-          value={value}
-          onChangeText={onChange}
-          placeholder={placeholder}
-          secureTextEntry={secureTextEntry}
-          keyboardType={KeyboardType}
-        />
-      )
-      }
-      
-      />
+        <>
+          <AppTextInput
+            value={value}
+            onChangeText={onChange}
+            placeholder={placeholder}
+            secureTextEntry={secureTextEntry}
+            keyboardType={KeyboardType}
+            style={error && styles.errorInput}
+          />
+          {error && (
+            <AppText style={styles.errorMessage}>{error?.message}</AppText>
+          )}
+        </>
+      )}
+    />
   );
 };
 
 export default AppTextInputController;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  errorInput: {
+    borderColor: AppColors.Red,
+  },
+  errorMessage: {
+    textAlign: "center",
+    color: AppColors.Red,
+    marginBottom: vs(10),
+    marginTop: vs(-7),
+  },
+});
