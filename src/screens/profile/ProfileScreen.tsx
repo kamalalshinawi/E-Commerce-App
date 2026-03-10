cbd61fd8e5b95e945eefacccfa73cf5b4e6ac9f8
kamalalshinawi <kamalalshinawi@gmail.com>
2026-03-11 01:21:55 +0200

    chor: clean some code

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import AppSafeView from "../../components/views/AppSafeView";
 import HomeHeader from "../../components/headers/HomeHeader";
