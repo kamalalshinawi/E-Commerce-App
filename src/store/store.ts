b7a2ecf3ee279c88599f4fb6e3ad6e2d2bdfd0b5
kamalalshinawi <kamalalshinawi@gmail.com>
2026-03-17 23:58:56 +0200

    fix: fix redux persist issue

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userData from "./reducers/UserSlice";
 import { persistCartSlice } from "./persisted/persistConfig";
-import persistStore from "redux-persist/es/persistStore";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 export const store = configureStore({
   reducer: {
     cartSlice: persistCartSlice,
     UserSlice: userData.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
